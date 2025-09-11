057124fd5fac4dfd0d6c9d7595067b1f7024ba47
Leon Ayral <leon.ayral@epita.fr>
2025-09-11 14:14:08 +0200

    fix: clean main.py

--- level_0/main.py
+++ level_0/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 
 app = FastAPI()
+
+class HouseFeatures(BaseModel):
+    size: int
+    nb_rooms: int
+    garden: int
 
 @app.get("/predict")
 def predict_get():
     return {"y_pred": 2}
 
 @app.post("/predict")
-def predict_post(data: dict):
+def predict_post(data: HouseFeatures):
     model = joblib.load("regression.joblib")
-    input_data = [[data["size"], data["nb_rooms"], data["garden"]]]
+    input_data = [[data.size, data.nb_rooms, data.garden]]
     prediction = model.predict(input_data)
 
     return {"y_pred": float(prediction[0])}
