f27141806a15befec6074b8d42a598a2e738aad9
Leon Ayral <leon.ayral@epita.fr>
2025-10-23 19:25:00 +0200

    clean code for level 0

--- level_0/tests.py
+++ level_0/tests.py
@@ -1,12 +1,5 @@
-# import requests
-# r = requests.get("http://127.0.0.1:8000/predict")
-# print(r.json())  # {'y_pred': 2}
-
 import requests
-data = {
-  "size": 1000,
-  "nb_rooms": 1,
-  "garden": 1
-}
-r = requests.post("http://4.210.225.87:5045/predict", json=data)
+
+data = {"size": 1000, "nb_rooms": 1, "garden": 1}
+r = requests.post("http://4.210.225.87:8000/predict", json=data)
 print(r.json())
